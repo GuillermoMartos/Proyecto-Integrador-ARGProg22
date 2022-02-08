e310784cd2665b9b4309498e87af6f54376e3bf4
Guillermo L. Martos <guille.l.martos@gmail.com>
2022-02-08 16:43:20 -0300

    agrego formulario de registro

--- Proyecto-Integrador-Profiler-App/src/app/app.component.ts
+++ Proyecto-Integrador-Profiler-App/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'Proyecto-Integrador-Profiler-App';
+  title = 'Landing-Profiler-App';
+  
+    registrationForm = new FormGroup({
+    name: new FormControl(null, Validators.required),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, Validators.required),
+    repeatPassword: new FormControl(null, Validators.required),
+  });
+  
+
+
+  registerUser(){
+    if (this.registrationForm.get('password')?.value !== this.registrationForm.get('repeatPassword')?.value) return alert("Passwords didn't match")
+  }
 }
