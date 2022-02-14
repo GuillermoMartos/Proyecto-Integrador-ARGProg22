093e702fa0d4c2c125d978a40c739cfa80c20997
Guillermo L. Martos <guille.l.martos@gmail.com>
2022-02-14 00:39:24 -0300

    back conectado!

---
+++ Backend-Profiler-App/db.js
@@ -0,0 +1,21 @@
+const mysql= require('mysql');
+const {database}=require('./mysql')
+const{promisify}=require('util')
+
+
+const pool= mysql.createPool(database)
+
+pool.getConnection((err, conn)=>{
+  if(err){
+    console.log(err.code)
+  }
+  if(conn){
+    conn.release()
+    console.log("DB connected!")
+  } 
+  return;
+})
+
+pool.query = promisify(pool.query)
+
+module.exports=pool
